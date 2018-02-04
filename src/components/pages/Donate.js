f52c0f37e2410895d1deb9165c198da21b5aeea1
Zach Alam <zelphics@gmail.com>
2018-02-03 16:05:38 -0800

    semantic ui local

--- src/components/pages/Donate.js
+++ src/components/pages/Donate.js
@@ -8,9 +8,9 @@ import MasterConfig from '../../config/Master'
 const Donate = () => (
   <div>
     <img src={qrdonate} style={{width:'100%'}} alt={'QR Donation Code'} />
-    <h1>Donations</h1>
+    <h1>Donate IOTA</h1>
     Help support and encourage the development of {MasterConfig.wallet} by
-    contributing to our own encrypted cold storage wallet!
+    contributing to our own encrypted cold wallet!
     <div style={{paddingTop:'1em', paddingBottom:'1em'}}>
 
     <TextArea key={'donation'} disabled style={{width:'100%', height:75}}>XQPVRLTJUVVIBWSFYEVSNDCJRCOOHIJKSYMHFYMWAZMLRGGKMUBLSPNNMQSUHKGL9BJGWWTLQATUQHLDZTLWJGZYUY</TextArea>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 
 import App from './components/structure/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 
