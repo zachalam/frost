80b1e0bf84e95ca193811c87d492b958920fb8d6
Zach Alam <zelphics@gmail.com>
2018-02-17 01:42:34 -0800

    offline tooltip

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-import { Icon, Message, Button } from 'semantic-ui-react'
+import { Icon, Message, Button, Popup } from 'semantic-ui-react'
 import vault from '../../images/vault.svg'
 import MasterConfig from '../../config/Master'
 
@@ -32,15 +32,20 @@ class Home extends Component {
    return (<div>
       <img src={vault} style={{width:'100%'}} alt={'IOTA Vault'} />
       <h1>Hello There!</h1>
-      Safely store coins offline. {MasterConfig.wallet} generates and reads
-      encrypted {MasterConfig.currency} seeds.
-      <div style={{paddingTop:'1em', paddingBottom:'1em'}}>
+      Safely store your {MasterConfig.currency} offline. {MasterConfig.wallet} generates and reads
+      encrypted wallets.
+      <div style={{paddingTop:'1.5em', paddingBottom:'1em'}}>
         <Button.Group>
-          <Button size={"large"} as={Link} to='/unlock'>Open Wallet</Button>
+          <Button size={"large"} as={Link} to='/unlock'>Unlock</Button>
           <Button.Or />
-          <Button size={"large"} positive as={Link} to='/start'>New Wallet</Button>
+          <Button size={"large"} positive as={Link} to='/start'>+New Wallet</Button>
         </Button.Group>
-        {this.state.online ? <Message warning><Icon name="alarm" />Go <u>offline</u> before using!</Message> : null}
+        {this.state.online ?
+          <Popup
+            trigger={<Message warning><Icon name="warning sign" />Go <u>offline</u> before using!</Message>}
+            content="Protect your wallet and password from inadvertently being shared."
+            inverted
+          /> : null}
       </div>
     </div>)
   }
