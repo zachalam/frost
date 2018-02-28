9e70a52ba90b9a5d6b155467383d5241a3a5106c
Zach Alam <zelphics@gmail.com>
2018-02-27 20:02:25 -0800

    force offline usage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -31,26 +31,28 @@ class Home extends Component {
   render() {
    return (<div>
       <img src={vault} style={{width:'100%'}} alt={'IOTA Vault'} />
-      <h1>Hello There!</h1>
-      Safely store your {MasterConfig.currency} offline. {MasterConfig.wallet} generates and reads
+      <h1>{MasterConfig.currency} Vault</h1>
+      Safely store coins offline. <b>{MasterConfig.wallet}</b> generates and reads
       encrypted wallets.
       <div style={{paddingTop:'1.5em', paddingBottom:'1em'}}>
-        <Button.Group>
-          <Button size={"large"} as={Link} to='/unlock'>Unlock</Button>
-          <Button.Or />
-          <Button size={"large"} positive as={Link} to='/start'>+New Wallet</Button>
-        </Button.Group>
         {this.state.online ?
           <Popup
             trigger={<Message warning><Icon name="warning sign" />Go <u>offline</u> before using!</Message>}
-            content="Protect your wallet and password from inadvertently being shared."
+            content={`You must disconnect from the Internet before using ${MasterConfig.wallet}.`}
             inverted
           /> :
-          <Popup
-            trigger={<Message positive><Icon name="check circle" />No Internet detected.</Message>}
-            content="Be secure. Stay offline when generating a cold wallet."
-            inverted
-          />}
+          <div>
+            <Button.Group>
+              <Button size={"large"} as={Link} to='/unlock'>Unlock</Button>
+              <Button.Or />
+              <Button size={"large"} positive as={Link} to='/start'>+New Wallet</Button>
+            </Button.Group>
+            <Popup
+              trigger={<Message positive><Icon name="check circle" />No Internet detected.</Message>}
+              content="Be secure. Stay offline when generating a cold wallet."
+              inverted
+            />
+          </div>}
       </div>
     </div>)
   }
