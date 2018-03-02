3e115a5e95524b458b71e6387a8e3e9809e09f61
Zach Alam <zelphics@gmail.com>
2018-03-01 22:25:15 -0800

    copy update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -32,12 +32,12 @@ class Home extends Component {
    return (<div>
       <img src={vault} style={{width:'100%'}} alt={'IOTA Vault'} />
       <h1>{MasterConfig.currency} Vault</h1>
-      Safely store coins offline. <b>{MasterConfig.wallet}</b> generates and reads
-      encrypted wallets.
+      Safely store coins. <b>{MasterConfig.wallet}</b> generates and reads
+      encrypted {MasterConfig.currency} wallets.
       <div style={{paddingTop:'1.5em', paddingBottom:'1em'}}>
         {this.state.online ?
           <Popup
-            trigger={<Message warning><Icon name="warning sign" />Go <u>offline</u> before using!</Message>}
+            trigger={<Message warning><Icon name="warning sign" />Offline use <b>required</b>!</Message>}
             content={`You must disconnect from the Internet before using ${MasterConfig.wallet}.`}
             inverted
           /> :
@@ -49,7 +49,7 @@ class Home extends Component {
             </Button.Group>
             <Popup
               trigger={<Message positive><Icon name="check circle" />No Internet detected.</Message>}
-              content="Be secure. Stay offline when generating a cold wallet."
+              content="Stay offline when generating or accessing cold wallets."
               inverted
             />
           </div>}
